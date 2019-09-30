33739f97e8338a7e93c2bd2c36906c23984a23ba
alucklydog <1848663638@qq.com>
2019-09-30 09:05:06 +0800

    修改阶乘及其逆元模板

--- 基础数学模版/组合数学预处理模板.cpp
+++ 基础数学模版/组合数学预处理模板.cpp
@@ -1,14 +1,16 @@
 const int maxn = 1e5 + 5;
 ll powmod(ll a, ll b) { ll res = 1; a %= mod; assert(b >= 0); for (; b; b >>= 1) { if (b & 1)res = res * a % mod; a = a * a % mod; }return res; }
-int fac[maxn];
-int invfac[maxn];
+ll fac[maxn];
+ll invfac[maxn];
 //预处理阶乘和阶乘的逆元
 void init()
 {
     fac[0] = 1;
-    for (int i = 1; i <= n; i++) fac[i] = 1LL * fac[i - 1] * i % mod;
-    invfac[n] = powmod(fac[n], mod - 2);
-    for (int i = n - 1; i >= 0; i--) invfac[i] = 1LL * invfac[i + 1] * (i + 1) % mod;
+    for (int i = 1; i < maxn; i++)
+        fac[i] = fac[i - 1] * i % mod;
+    invfac[maxn - 1] = powmod(fac[maxn - 1], mod - 2);
+    for (int i = maxn - 2; i >= 0; i--)
+        invfac[i] = invfac[i + 1] * (i + 1) % mod;
 }
 
-inline LL combi(int k, int n) { return k > n ? 0 : 1LL * fac[n] * invfac[k] % mod * invfac[n - k] % mod; }
+inline ll combi(int k, int n) { return k > n ? 0 : 1LL * fac[n] * invfac[k] % mod * invfac[n - k] % mod; }
