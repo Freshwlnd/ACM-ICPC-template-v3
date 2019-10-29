ea1e30bd2c1452969a43cac479389b36ef756e3b
Freshwlnd <31024123+Freshwlnd@users.noreply.github.com>
2019-10-29 15:22:45 +0800

    Update and rename 后缀自动机 to 后缀自动机.cpp

--- 字符串处理/后缀自动机.cpp
+++ 字符串处理/后缀自动机.cpp
@@ -1,22 +1,8 @@
 #include <bits/stdc++.h>
 #define per(i,a,n) for (int i=a;i<n;i++)
 #define rep(i,a,n) for (int i=n-1;i>=a;i--)
-#define pb push_back
-#define mp make_pair
-#define all(x) (x).begin(),(x).end()
-#define fi first
-#define se second
-#define SZ(x) ((int)(x).size())
-#define mem(a, b) memset(a, b, sizeof(a))
 using namespace std;
 typedef long long ll;
-typedef unsigned long long ull;
-typedef pair<int, int> pI;
-typedef vector<ll> vI;
-const ll mod(1e9 + 7);
-const ll INF(0x3f3f3f3f);
-ll powmod(ll a, ll b) { ll res = 1; a %= mod; assert(b >= 0); for (; b; b >>= 1) { if (b & 1)res = res * a%mod; a = a * a%mod; }return res; }
-ll gcd(ll a, ll b) { return b ? gcd(b, a%b) : a; }
 // head
 
 const int MAXL(1e6+7);
@@ -52,9 +38,9 @@ void topsort() {
 }
 
 int main() {
-    scanf("%s",s+1);
+    cin >> s+1;
     len = strlen(s+1)+1;cnt = last = 1; //字符串[1,len)
-    per(i,1,len) extand(s[i]-'a');
+    per(i, 1, len) extand(s[i]-'a');
     //计算每个长度的子串出现最多的次数
     topsort();  //拓扑排序
     per(i,1,cnt+1) ans[mx[i]] = max(ans[mx[i]],R[i]);
