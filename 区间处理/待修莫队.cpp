67c72a9191d40c73eb5aaf8e2f68396be1b33700
alucklydog <1848663638@qq.com>
2019-10-30 16:23:48 +0800

    待修莫队

--- 区间处理/待修莫队.cpp
+++ 区间处理/待修莫队.cpp
@@ -1,17 +1,17 @@
 #pragma warning(disable:4996)
-//��Ŀ��hdoj6610 http://acm.hdu.edu.cn/showproblem.php?pid=6610
-//���⣺��һ��ʯ������Nim���ġ�����Alice����ѡ��һ������[L,R]��Bob�����������ѡ��һ��������[l,r]������Aliceѡ���[L,R]�ڴ��ڶ��ٸ�������ʹ��Alice��ʤ(Alice����)
-//Bob��Ȩ����ÿ�β��Ŀ�ʼǰѡ��һ���㣬ʹ�õ��������Ǹ��㽻��ʯ��������swap(a[i],a[i+1])
+//题目：hdoj6610 http://acm.hdu.edu.cn/showproblem.php?pid=6610
+//题意：在一堆石子中玩Nim博弈。但是Alice会先选择一个区间[L,R]，Bob在这个区间内选择一个子区间[l,r]，问在Alice选择的[L,R]内存在多少个子区间使得Alice必胜(Alice先行)
+//Bob有权利在每次博弈开始前选择一个点，使该点和其后面那个点交换石堆数。即swap(a[i],a[i+1])
 
-//����Nim�����������Ͳ�Ϊ0��ô�Ϳ��Ա�ʤ����ô��ͳ�Ʊذܡ��ܹ���(R-L+1)*(R-L+2)/2�������䡣
-//���ǵ�ÿ�ζ����������TLE������Ҫ�Ż�һ�¡��������в�ѯ[L,R]û��ǿ�����߿��Կ��������㷨��
-//������Ϊ���޸Ĳ�������ʹ��Ī�����޷�ʵ�ֵġ���ϸ˼�������޸Ĳ�����ֻӰ�����������㡣
-//��������Ҫ����[L,R]������Ϊ0���������������ô���԰�����ת������[L,R]��������ͬǰ׺������ͬ����������
-//��Ϊʹ��ǰ׺�͵�ԭ����ʵӦ��ͳ��[L - 1,R]�����ڵĶ���
-//��ô����Bob���޸���ʵֻӰ����pre[i](ǰ׺����pre)��ֵ
-//�����޸Ŀ���ʹ�ô���Ī����ɣ����޸�ʱ����Ϊ����ά��
+//根据Nim博弈区间异或和不为0那么就可以必胜，那么先统计必败。总共有(R-L+1)*(R-L+2)/2个子区间。
+//考虑到每次都暴力计算会TLE，就需要优化一下。对于所有查询[L,R]没有强制在线可以考虑离线算法。
+//但是因为有修改操作单独使用莫队是无法实现的。仔细思考题中修改操作，只影响了这两个点。
+//现在我们要求在[L,R]中异或和为0的子区间个数，那么可以把问题转化成在[L,R]区间内相同前缀异或和相同的数对数。
+//因为使用前缀和的原因其实应当统计[L - 1,R]区间内的对数
+//那么现在Bob的修改其实只影响了pre[i](前缀异或和pre)的值
+//单点修改可以使用待修莫队完成，以修改时间作为第三维度
 
-//ע��Ī�Ӷ�ÿһ��ά�ȶ���Ҫ�ֿ飬��άĪ�ӷֿ���pow(n,0.5),��άĪ�ӷֿ�Ӧ����pow(n,2/3.0)��֤����ѧϰ���ӣ�https://www.cnblogs.com/ouuan/p/MoDuiTutorial.html (��л���еĲ�������ѧ����άĪ��)
+//注意莫队对每一个维度都需要分块，二维莫队分块是pow(n,0.5),三维莫队分块应该是pow(n,2/3.0)。证明及学习链接：https://www.cnblogs.com/ouuan/p/MoDuiTutorial.html (感谢大佬的博客让我学会三维莫队)
 #include<bits/stdc++.h>
 #define upf(a,b,c) for(ll a=b;a<=c;++a)
 #define drf(a,b,c) for(ll a=b;a>=c;--a)
@@ -19,7 +19,6 @@
 #define Tcase(n) for(int _=1;_<=n;++_)
 #define ll long long
 #define ull unsigned long long
-#define ShaShiBuGan int woyebuzhidaogaiganmasuoyishenmedoubugan=0;
 
 #if 1
 #define endl '\n'
@@ -35,19 +34,19 @@ ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
 int T;
 int n, m, a[maxn], unit, pre[maxn], l, r;
 ll trueans[maxn], ans;
-//sumά������[L,R]������ÿ�����ֳ��ִ������������ÿһ�ε�ans
-int sum[1024 * 1024 + 5];//a[i]<=1e6�������Ϊ1024*1024-1
-//����ѯ�ʣ���������
+//sum维护了在[L,R]区间内每个数字出现次数，方便计算每一次的ans
+int sum[1024 * 1024 + 5];//a[i]<=1e6最大异或和为1024*1024-1
+//保存询问，离线排序
 struct questions {
     int l, r, t, id, pos;
     bool operator < (const questions& a) {
         return l / unit == a.l / unit ? (r / unit == a.r / unit ? t < a.t : r < a.r) : l < a.l;
     }
 }q[maxn];
-vector<questions> v;//v�б�����ÿһ���޸ģ���¼Ϊ��ǰʱ�䡣
-//����ָ��
+vector<questions> v;//v中保存了每一次修改，记录为当前时间。
+//右移指针
 inline void add(int& x, int opt) {
-    if (x == 0 && opt == 1) { x++; return; }//L�ӳ�ʼ����0�ƶ���1ʱ��û�м��ٶ���(��ʵ����Ϊpre��ʼ��Ϊ0�����⣬��ʼ��Ϊ-1Ҳ��Ҫ����)
+    if (x == 0 && opt == 1) { x++; return; }//L从初始化的0移动到1时并没有减少东西(其实是因为pre初始化为0有问题，初始化为-1也需要特判)
     //l++
     if (opt == 1) {
         sum[pre[x]]--;
@@ -56,12 +55,12 @@ inline void add(int& x, int opt) {
     //r++
     else {
         ans += sum[pre[++x]];
-        sum[pre[x]]++;//ǰ׺��������a[x]���ִ���
+        sum[pre[x]]++;//前缀和区间内a[x]出现次数
     }
 }
-//����ָ��
+//左移指针
 inline void sub(int& x, int opt) {
-    if (x == 1 && opt == 1) { x--; return; }//������ָ��ͬ�����������ƺ���û����
+    if (x == 1 && opt == 1) { x--; return; }//与右移指针同理，但是左移好像并没有用
     if (opt == 1) {
         ans += sum[pre[--x]];
         sum[pre[x]]++;
@@ -71,27 +70,27 @@ inline void sub(int& x, int opt) {
         ans -= sum[pre[x--]];
     }
 }
-//1��������״̬��2��ǰ����״̬
+//1往后推移状态，2往前推移状态
 inline void modify(int& time, int opt) {
-    if (opt == 1) time++;//�������״̬��Ҫ�ȱ�ɺ����ʱ��
+    if (opt == 1) time++;//向后推移状态需要先变成后面的时间
     int pos = v[time].id;
-    if (l <= pos && pos <= r) {//�޸ĵ��ڵ�ǰ[L,R]֮ǰ��Ҫ���޸�Ӱ������ȥ
-        //��ȥ�޸�״̬ǰ��ֵ
+    if (l <= pos && pos <= r) {//修改点在当前[L,R]之前需要将修改影响结果减去
+        //减去修改状态前的值
         sum[pre[pos]]--;
         ans -= sum[pre[pos]];
-        //�޸�״̬
+        //修改状态
         pre[pos] = pre[pos] ^ a[pos] ^ a[pos + 1];
         swap(a[pos], a[pos + 1]);
-        //�����޸�״̬���ֵ
+        //加上修改状态后的值
         ans += sum[pre[pos]];
         sum[pre[pos]]++;
     }
-    //��Ӱ���ֱ�Ӵ���pre������
+    //不影响就直接处理pre就行了
     else {
         pre[pos] = pre[pos] ^ a[pos] ^ a[pos + 1];
         swap(a[pos], a[pos + 1]);
     }
-    if (opt == 2) time--;//��ǰ�ƶ�״̬��Ҫ������������ʱ��
+    if (opt == 2) time--;//向前移动状态需要先运算再左移时间
 }
 int main() {
 #ifdef ACM_LOCAL
@@ -104,25 +103,25 @@ int main() {
 
     while (cin >> n >> m) {
         v.clear();
-        v.push_back(q[0]);//��ֵ
-        unit = pow(n, 2 / 3.0);//��ʼ���ֿ��С
+        v.push_back(q[0]);//空值
+        unit = pow(n, 2 / 3.0);//初始化分块大小
         upf(i, 1, n) {
             cin >> a[i];
-            pre[i] = pre[i - 1] ^ a[i];//��ǰ׺��
+            pre[i] = pre[i - 1] ^ a[i];//求前缀和
         }
         int time = 0;
         upf(i, 1, m) {
             int op;
             cin >> op;
-            //��¼pos����Ϊֻ��op=1ʱ�Ż�����𰸣���q�Ѿ����Ź���������Ҫ��¼pos
-            if (op == 1) {//op=1ʱΪ��ѯ
+            //记录pos是因为只有op=1时才会输出答案，而q已经被排过序所以需要记录pos
+            if (op == 1) {//op=1时为查询
                 cin >> q[i].l >> q[i].r;
                 q[i].t = time;
                 q[i].id = 0;
                 q[i].pos = i;
             }
-            else {//op=2�����޸�
-                cin >> q[i].id;//Bobѡ��ĵ�
+            else {//op=2单点修改
+                cin >> q[i].id;//Bob选择的点
                 q[i].l = q[i].r = 0;
                 q[i].t = ++time;
                 q[i].pos = i;
@@ -130,30 +129,30 @@ int main() {
             }
         }
         sort(q + 1, q + 1 + m);
-        //��ʼ��
+        //初始化
         l = 0, r = 0, ans = 0, time = 0;
         upf(i, 0, m)trueans[i] = -1;
         //sum[0] = 1;
         upf(i, 1, m) {
-            //ʱ��״̬��Ҫ�޸�
+            //时间状态需要修改
             while (time < q[i].t)modify(time, 1);
             while (time > q[i].t)modify(time, 2);
-            if (q[i].id != 0)//��ǰѯ����Bob���޸ĵĻ�����Ҫ����l��r���ƶ�
+            if (q[i].id != 0)//当前询问是Bob的修改的话不需要进行l和r的移动
                 continue;
-            while (r < q[i].r)add(r, 2);//���ƺ���
-            while (r > q[i].r)sub(r, 2);//���ƺ���
+            while (r < q[i].r)add(r, 2);//右移函数
+            while (r > q[i].r)sub(r, 2);//左移函数
             while (l < q[i].l)add(l, 1);
             while (l > q[i].l)sub(l, 1);
-            ans += sum[pre[l - 1]];//������L-1�����
+            ans += sum[pre[l - 1]];//计算上L-1这个点
             trueans[q[i].pos] = (ll)(r - l + 2) * (r - l + 1) / 2 - ans;
-            ans -= sum[pre[l - 1]];//��ȥL-1�����
+            ans -= sum[pre[l - 1]];//除去L-1这个点
         }
         upf(i, 1, m) {
-            if (trueans[i] != -1) {//���޸���Ҫ���
+            if (trueans[i] != -1) {//非修改需要输出
                 cout << trueans[i] << '\n';
             }
         }
-        //��ʼ��sum
+        //初始化sum
         upf(i, l, r) {
             sum[pre[i]]--;
         }
